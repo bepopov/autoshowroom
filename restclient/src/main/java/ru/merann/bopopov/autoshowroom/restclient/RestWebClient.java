aeb5a7619a90b23393d45856099c9124d955adf5
Bogdan Popov <ibogdanpopov@gmail.com>
2019-04-18 12:39:53 +0300

    Delete jsf dependencies

--- restclient/src/main/java/ru/merann/bopopov/autoshowroom/restclient/RestWebClient.java
+++ restclient/src/main/java/ru/merann/bopopov/autoshowroom/restclient/RestWebClient.java
@@ -1,21 +1,10 @@
 package ru.merann.bopopov.autoshowroom.restclient;
 
-import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
-
-@EnableAutoConfiguration
-@ComponentScan("ru.merann.bopopov.autoshowroom.restclient")
-public class RestWebClient extends SpringBootServletInitializer {
+@SpringBootApplication
+public class RestWebClient {
 
 
 
@@ -36,21 +25,6 @@ public class RestWebClient extends SpringBootServletInitializer {
         ResultListOrder listOrder = clientService.getOrdersByStatus(Status.ACCEPTED, 1L);
         System.out.println(listOrder.toString());
         */
-    }
-
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        return new ServletRegistrationBean(servlet, "*.jsf");
-    }
-
-    @Bean
-    public FilterRegistrationBean rewriteFilter() {
-        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
-        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
-                DispatcherType.ASYNC, DispatcherType.ERROR));
-        rwFilter.addUrlPatterns("/*");
-        return rwFilter;
     }
 
 }
