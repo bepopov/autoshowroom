015845dcd747266ccc24b84a1811067a1e2d4cfb
Bogdan Popov <ibogdanpopov@gmail.com>
2019-04-19 14:13:24 +0300

    Small fix

--- restclient/src/main/java/ru/merann/bopopov/autoshowroom/restclient/RestWebClient.java
+++ restclient/src/main/java/ru/merann/bopopov/autoshowroom/restclient/RestWebClient.java
@@ -1,11 +1,37 @@
 package ru.merann.bopopov.autoshowroom.restclient;
 
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-@SpringBootApplication
-public class RestWebClient {
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+
+@EnableAutoConfiguration
+@ComponentScan("ru.merann.bopopov.autoshowroom.restclient")
+public class RestWebClient extends SpringBootServletInitializer {
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean(servlet, "*.jsf");
+    }
+
+    @Bean
+    public FilterRegistrationBean rewriteFilter() {
+        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+                DispatcherType.ASYNC, DispatcherType.ERROR));
+        rwFilter.addUrlPatterns("/*");
+        return rwFilter;
+    }
 
 
     public static void main(String[] args) {
